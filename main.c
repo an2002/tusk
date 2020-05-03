ccf8df14a7254ed0b552796dbd6794634097c9f5
an2002 <64723127+an2002@users.noreply.github.com>
2020-05-03 16:25:40 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Determine ages in respect of 12,18,24.
+int main()
+{
+    /*printf("please enter the age ");
+    int age;
+    scanf("%d",&age);
+    if (age>18){
+        printf("the age is greater than 18\n");
+    if (age<24){
+        printf("the age is greater than 18 but less than 24");
+        }
+    else {
+        printf("the age is greater than 18 but not less than 24\n");
+        }
+    }
+    else if(age==24){
+        printf("the age is equal to 24");
+    }
+    else if(age==18){
+        printf("the age is equal to 18");
+    }
+    else if(age<18){
+        printf("the age is less than 18\n");
+    if(age>12){
+        printf("the age is less than 18 but greater than 12");
+        }
+    else{
+        printf("the age is less than 18 but not greater than 12\n");
+        }
+   }
+    else if(age=12){
+        printf("the age is equal to 12");
+   }
+   getch();*/
+   int a;
+   scanf("%d",&a);
+   int b=a%2;
+   (  b=0  ) ?  printf("even") : printf("odd");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
